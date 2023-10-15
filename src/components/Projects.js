2a3da8dd5fca47b8324fb7b7148af2332a15aa2e
sirGentlejack <fagbemiilemobayo@gmail.com>
2023-10-16 00:13:11 +0100

    added alt in img tags

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,28 +8,28 @@ export default function Projects() {
       <div className="grid-container">
         <div className="project">
           <a href="https://lively-strudel-dd7f7a.netlify.app/" target="_blank" rel="noreferrer">
-            <img src={screenshot} className="project-img"  />
+            <img src={screenshot} className="project-img" alt="screenshot showing project" />
             <br />
             <span>Opay Demo</span>
           </a>
         </div>
         <div className="project" >
           <a href="https://clinquant-alpaca-76679d.netlify.app/#" target="_blank" rel="noreferrer">
-            <img src={screenshot} className="project-img"  />
+            <img src={screenshot} className="project-img" alt="screenshot showing project" />
             <br />
             <span>Real Estate</span>
           </a>
         </div>
         <div className="project">
           <a href="https://stellar-croissant-74a254.netlify.app/" target="_blank" rel="noreferrer">
-            <img src={screenshot} className="project-img"  />
+            <img src={screenshot} className="project-img" alt="screenshot showing project" />
             <br />
             <span className="project-name">Loan calculator</span>
           </a>
         </div>
         <div className="project">
           <a href="https://roaring-valkyrie-539f49.netlify.app/" target="_blank" rel="noreferrer">
-            <img src={screenshot} className="project-img"  />
+            <img src={screenshot} className="project-img" alt="screenshot showing project" />
             <br />
             <span>BMI calculator</span>
           </a>
