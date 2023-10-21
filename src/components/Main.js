0f2e6b22c625b883c87f35d3a876cde7a5849cc6
sirGentlejack <fagbemiilemobayo@gmail.com>
2023-10-21 21:57:20 +0100

    Bg color change

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,11 +3,28 @@ import React from "react";
 export default function Main() {
   return (
     <div>
-      <h3 className="sub-topic">Interest</h3>
-      <p className="interest-content">
-        I am passionate about continuous learning, problem-solving, and
-        contributing to an organisation’s growth while aligning with its values.
-      </p>
+      <div>
+        <h3 className="sub-topic">Interest</h3>
+        <p className="interest-content">
+          I am passionate about continuous learning, problem-solving, and
+          contributing to an organisation’s growth while aligning with its
+          values.
+        </p>
+      </div>
+      <div>
+        <h3 className="sub-topic">Technologies</h3>
+        <ul className="technologies">
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>javaScript</li>
+          <li>Reactjs</li>
+          <li>TailwindCSS</li>
+          <li>Git</li>
+          <li>Responsive design</li>
+          <li>Slack</li>
+          <li>Figma</li>
+        </ul>
+      </div>
     </div>
   );
 }
